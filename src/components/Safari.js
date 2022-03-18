bacf1ec82eab6977ad73626658f200200b4262e7
Jared <wilbrahamjared@gmail.com>
2022-03-18 16:35:57 -0400

    Completed Safari Browser

--- src/components/Safari.js
+++ src/components/Safari.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import Close from '../images/safari/Close.png'
-import Nav from '../images/safari/nav.png'
-import Minimize from '../images/safari/Minimize.png'
-import Zoom from '../images/safari/Zoom.png'
+import React from "react";
+import Close from "../images/safari/Close.png";
+import Nav from "../images/safari/nav.png";
+import Minimize from "../images/safari/Minimize.png";
+import Zoom from "../images/safari/Zoom.png";
+import Refresh from "../images/safari/refresh.png";
+import NewTab from "../images/safari/new-tab.png";
+import TabOverview from "../images/safari/tab-overview.png";
+import Share from "../images/safari/share.png";
+import VacationHome from "../images/safari/vacation-home.jpg";
+import AirBnb from "../images/airbnb-logo.png";
 
 export default function Safari() {
-    return (
-        <div className="safari-container">
-            <div className="safari--header">
-                <img src={Close} />
-                <img src={Minimize} />
-                <img src={Zoom} />
-                <img src={Nav} />
-            </div>
-
+  return (
+    <div className="safari-container">
+      <div className="safari--header">
+        <div className="safari--buttons">
+          <img src={Close} />
+          <img src={Minimize} />
+          <img src={Zoom} />
         </div>
-    )
+        <img className="safari--nav" src={Nav} />
+        <div className="safari--domain">
+          airbnb.com/experiences
+          <img src={Refresh} className="safari--refresh" />
+        </div>
+        <div className="safari--corner">
+          <img src={Share} />
+          <img src={NewTab} />
+          <img src={TabOverview} />
+        </div>
+      </div>
+
+      <div className="safari--body">
+        <img className="safari--airlogo" src={AirBnb} />
+        <div className="safari--hero">
+          <div className="safari-content">
+            <h1>Book a home near your experience</h1>
+            <button className="buttons">Book Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
